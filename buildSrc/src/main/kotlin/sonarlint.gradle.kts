c772fff43128210968aa2290ebdcc46242a26f41
Fiereu <contact@fiereu.de>
2026-07-21 11:01:59 +0200

    chore(lint): stop sonarlint from flagging TODO comments (#51)

--- buildSrc/src/main/kotlin/sonarlint.gradle.kts
+++ buildSrc/src/main/kotlin/sonarlint.gradle.kts
@@ -7,5 +7,7 @@ sonarLint {
   rules {
     disable("kotlin:S1186")
     disable("kotlin:S3776")
+    // The todo-to-issue workflow already tracks open tasks, so lint must not flag them too.
+    disable("kotlin:S1135")
   }
 }
