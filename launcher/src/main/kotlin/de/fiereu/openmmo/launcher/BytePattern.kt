f1c34416442b40954d14b731c5c8baa8cc670b17
Fiereu <contact@fiereu.de>
2026-08-11 10:56:33 +0200

    feat(launcher): add a patch type that finds code by byte signature (#165)

---
+++ launcher/src/main/kotlin/de/fiereu/openmmo/launcher/BytePattern.kt
@@ -0,0 +1,3 @@
+Removed on request of pokemmo.eu.
+Date: 2026-08-17
+Nonce: fd82ba5bd34241f4909cc100aa27b9dd
---
+++ launcher/src/test/kotlin/de/fiereu/openmmo/launcher/BytePatternTest.kt
@@ -0,0 +1,3 @@
+Removed on request of pokemmo.eu.
+Date: 2026-08-17
+Nonce: 8210883fd0644c99bc182ee9ffb60ba1
